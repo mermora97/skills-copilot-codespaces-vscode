558c2e63105806191d6fa13e4d9ccefcbea7489d
Mercedes Mora-Figueroa de Liñán <56823677+mermora97@users.noreply.github.com>
2023-12-01 11:54:21 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMember(){
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Python'];
+    var member = {
+        name: 'John',
+        age: 20,
+        skills: skills,
+        // skills: ['HTML', 'CSS', 'JS', 'React', 'Python'],
+        // skills: ['HTML', 'CSS', 'JS', 'React', 'Python'],
+        // skills: ['HTML', 'CSS', 'JS', 'React', 'Python'],
+        // skills: ['HTML', 'CSS', 'JS', 'React', 'Python'],
+        // skills: ['HTML', 'CSS', 'JS', 'React', 'Python'],
+    }
+    console.log(member.skills);
+    console.log(member.skills[1]);
+    console.log(member.skills[4]);
+}
